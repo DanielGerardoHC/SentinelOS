0c563829a291a377bd2d9db19e43d13ea95975ff
DanielGerardoHC <gerardoclm555@gmail.com>
2026-03-12 23:58:29 -0600

    Method to get routes fixed

--- core/internal/system/routes.go
+++ core/internal/system/routes.go
@@ -1,6 +1,7 @@
 package system
 
 type RouteInfo struct {
+	ID          int    `json:"id"`
 	Destination string `json:"destination"`
 	Gateway     string `json:"gateway"`
 	Interface   string `json:"interface"`
@@ -10,19 +11,20 @@ type RouteInfo struct {
 
 func GetRoutes() ([]RouteInfo, error) {
 
-fw := GetFirewall()
-if fw == nil {
-	return nil, ErrFirewallNotInitialized
-}
+	fw := GetFirewall()
+	if fw == nil {
+		return nil, ErrFirewallNotInitialized
+	}
 
 	var out []RouteInfo
 
 	for _, route := range fw.Routes {
 		out = append(out, RouteInfo{
+			ID:          route.ID,
 			Destination: route.Destination,
-			Gateway: route.Gateway,
-			Interface: route.Interface,
-			Metric: route.Metric,
+			Gateway:     route.Gateway,
+			Interface:   route.Interface,
+			Metric:      route.Metric,
 			Description: route.Description,
 		})
 	}
